b4e98c63a712c845a91cb7fb7d00ba695d373e6b
Russel Winder <russel@winder.org.uk>
2015-12-19 17:07:28 +0000

    Update version and reformat a bit.

--- Frege/project.clj
+++ Frege/project.clj
@@ -4,8 +4,9 @@
   :license {:name "GPL v3"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [
-                 [org.frege-lang/frege "3.23.443-gad63824"]]
-  :repositories {"clojars" "https://clojars.org/org.frege-lang/frege"
+                 [org.frege-lang/frege "3.23.450-SNAPSHOT"]]
+  :repositories {
+                 "clojars" "https://clojars.org/org.frege-lang/frege"
                  "sonatype" "https://oss.sonatype.org/content/repositories/snapshots/"}
   :plugins [[lein-fregec "3.23.450"]]
   :frege-source-paths ["src" "test"]

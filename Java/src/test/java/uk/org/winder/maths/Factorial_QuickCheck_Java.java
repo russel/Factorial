49cb4df581d4dc8cec5454b260ba458066b6294f
Russel Winder <russel@winder.org.uk>
2015-12-21 11:56:34 +0000

    Make the JUnit-Quickcheck bits much better.

--- Java/src/test/java/uk/org/winder/maths/Factorial_QuickCheck_Java.java
+++ Java/src/test/java/uk/org/winder/maths/Factorial_QuickCheck_Java.java
@@ -1,10 +1,13 @@
 package uk.org.winder.maths;
 
 import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import com.pholser.junit.quickcheck.generator.InRange;
 import com.pholser.junit.quickcheck.Property;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.assertEquals;
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
 
 import java.math.BigInteger;
 
@@ -17,40 +20,46 @@ import static uk.org.winder.maths.Factorial.reductive;
 @RunWith(JUnitQuickcheck.class)
 public class Factorial_QuickCheck_Java {
 
-  // Use byte as a way of restricting the samples to the range [-128, 127].
+  // Keep the Integer argument range relatively small so that the tests run in reasonable time.  [1, 500] is
+  // seen as reasonable.
 
-  @Property public void iterative_positive(final Byte i) {
-    if (i > 0) {
-      assertEquals(iterative(i), BigInteger.valueOf(i).multiply(iterative(i - 1)));
-    }
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Property public void iterative_positive(@InRange(min = "1", max = "500") final Integer i) {
+    assertEquals(iterative(i), BigInteger.valueOf(i).multiply(iterative(i - 1)));
   }
 
-  @Property public void reductive_positive(final Byte i) {
-    if (i > 0) {
-      assertEquals(reductive(i), BigInteger.valueOf(i).multiply(reductive(i - 1)));
-    }
+  @Property public void iterative_negative(@InRange(min = "-500", max = "-1") final Integer i) {
+    thrown.expect(RuntimeException.class);
+    iterative(i);
   }
 
-  /*
-   * naïveRecursion will generally run out of stack space for even moderate size arguments.
-   * Do not test it.
-
-  @Property public void naïveRecursive_positive(final Integer i) {
-    if (i > 0) {
-      assertEquals(naïveRecursive(i), BigInteger.valueOf(i).multiply(naïveRecursive(i - 1)));
-    }
+  @Property public void naïveRecursive_positive(@InRange(min="1", max="500") final Integer i) {
+    assertEquals(naïveRecursive(i), BigInteger.valueOf(i).multiply(naïveRecursive(i - 1)));
   }
-  */
 
-  /*
-   * Java does not support tail call optimization (at least not as of Java 8) so will suffer
-   * the same stack problems as naïveRecursive. Do not test it.
-
-  @Property public void tailRecursive_positive(final Integer i) {
-    if (i > 0) {
-      assertEquals(tailRecursive(i), BigInteger.valueOf(i).multiply(tailRecursive(i - 1)));
-    }
+  @Property public void naïveRecursive_negative(@InRange(min="-500", max="-1") final Integer i) {
+    thrown.expect(RuntimeException.class);
+    naïveRecursive(i);
   }
-  */
+
+  @Property public void tailRecursive_positive(@InRange(min="1", max="500") final Integer i) {
+    assertEquals(tailRecursive(i), BigInteger.valueOf(i).multiply(tailRecursive(i - 1)));
+  }
+
+  @Property public void tailRecursive_negative(@InRange(min="-500", max="-1") final Integer i) {
+    thrown.expect(RuntimeException.class);
+    tailRecursive(i);
+  }
+
+  @Property public void reductive_positive(@InRange(min="1", max="500") final Integer i) {
+    assertEquals(reductive(i), BigInteger.valueOf(i).multiply(reductive(i - 1)));
+  }
+
+  @Property public void reductive_negative(@InRange(min="-500", max="-1") final Integer i) {
+    thrown.expect(RuntimeException.class);
+    reductive(i);
+  }
 
 }

575889492558f28ae813088b655eb230d5d7ab2a
Russel Winder <russel@winder.org.uk>
2016-07-08 15:18:23 +0100

    Add KotlinTest for negative integer arguments.

--- Kotlin/src/test/kotlin/uk/org/winder/maths/factorial/Factorial_KotlinTest_PropertyBased.kt
+++ Kotlin/src/test/kotlin/uk/org/winder/maths/factorial/Factorial_KotlinTest_PropertyBased.kt
@@ -13,13 +13,29 @@ class Factorial_KotlinTest_PropertyBased : StringSpec() {
     )
 
     algorithms.forEach{a ->
+      val name = a.first
       val f = a.second
-      (a.first + ":recurrence relation is true for all non-negative integer values") {
+
+      (name + ": recurrence relation is true for all non-negative integer values") {
         forAll{i: Int ->
           if (0 < i && i < 500) { f(i) == (i.bigint * f(i - 1)) }
           else { true }
         }
       }
+
+      (name + ": negative argument cause an exception") {
+        // See https://github.com/kotlintest/kotlintest/issues/70
+        listOf(-100, -20, -2, -1).forEach {
+          shouldThrow<IllegalArgumentException> { f(it) }
+        }
+/*
+        forAll{i:Int ->
+          if (i < 0) { shouldThrow<IllegalArgumentException> { f(i) } }
+          else { true }
+        }
+*/
+      }
+
     }
 
   }

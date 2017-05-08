216b987a7ebac85966f12bf3ee4e951941a8ed01
Russel Winder <russel@winder.org.uk>
2017-05-08 14:45:12 +0100

    Revert to using the init method for tests as this is more Kotlinic.

--- Kotlin/src/test/kotlin/uk/org/winder/maths/factorial/Factorial_KotlinTest_PropertyBased.kt
+++ Kotlin/src/test/kotlin/uk/org/winder/maths/factorial/Factorial_KotlinTest_PropertyBased.kt
@@ -14,36 +14,38 @@ val smallishWholeNumbers = object : Gen<Int> {
     override fun generate() = random.nextInt(700)
 }
 
-class Factorial_KotlinTest_PropertyBased : StringSpec({
+class Factorial_KotlinTest_PropertyBased : StringSpec() {
+    init {
 
-    val algorithms = table(
-            headers("name", "f"),
-            row("iterative", { x: Int -> iterative(x) }),
-            row("reductive", { x: Int -> reductive(x) }),
-            row("foldive", { x: Int -> foldive(x) }),
-            row("naïve_recursive", { x: Int -> naïve_recursive(x) }),
-            row("tail_recursive", { x: Int -> tail_recursive(x) })
-    )
+        val algorithms = table(
+                headers("name", "f"),
+                row("iterative", { x: Int -> iterative(x) }),
+                row("reductive", { x: Int -> reductive(x) }),
+                row("foldive", { x: Int -> foldive(x) }),
+                row("naïve_recursive", { x: Int -> naïve_recursive(x) }),
+                row("tail_recursive", { x: Int -> tail_recursive(x) })
+        )
 
-    forAll(algorithms) { name, f ->
+        forAll(algorithms) { name, f ->
 
-        "$name: base case holds."  {
-            f(0) == 1.bigint
-        }
-
-        "$name: recurrence relation is true for non-negative integer values" {
-            forAll(smallishWholeNumbers) { i ->
-                f(i + 1) == (i + 1).bigint * f(i)
+            "$name: base case holds."  {
+                f(0) == 1.bigint
             }
-        }
 
-        "$name: negative argument cause an exception" {
-            forAll(Gen.negativeIntegers()) { i: Int ->
-                shouldThrow<IllegalArgumentException> { f(i) }
-                true
+            "$name: recurrence relation is true for non-negative integer values" {
+                forAll(smallishWholeNumbers) { i ->
+                    f(i + 1) == (i + 1).bigint * f(i)
+                }
             }
+
+            "$name: negative argument cause an exception" {
+                forAll(Gen.negativeIntegers()) { i: Int ->
+                    shouldThrow<IllegalArgumentException> { f(i) }
+                    true
+                }
+            }
+
         }
 
     }
-
-})
+}

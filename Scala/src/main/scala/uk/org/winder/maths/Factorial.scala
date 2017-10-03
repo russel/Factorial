a2f96e775a6760f1ce72388a7225eb01db387f91
Russel Winder <russel@winder.org.uk>
2017-10-03 07:59:40 +0100

    Switch to tab indent.

--- Scala/src/main/scala/uk/org/winder/maths/Factorial.scala
+++ Scala/src/main/scala/uk/org/winder/maths/Factorial.scala
@@ -3,86 +3,86 @@ package uk.org.winder.maths
 import scala.annotation.tailrec
 
 object Factorial {
-  val one = BigInt(1)
-  val two = BigInt(2)
+	val one = BigInt(1)
+	val two = BigInt(2)
 
-  private def validate(n:BigInt):Unit = {
-    if (n < 0) throw new IllegalArgumentException("Parameter must be a non-negative integer.")
-  }
+	private def validate(n:BigInt):Unit = {
+		if (n < 0) throw new IllegalArgumentException("Parameter must be a non-negative integer.")
+	}
 
-  def iterativeWhile(n:BigInt):BigInt = {
-    validate(n)
-    var i = n
-    var result = one
-    while (i > 0) {
-      result = result * i
-      i = i - one
-    }
-    result
-  }
-  def iterativeWhile(n:Int):BigInt = iterativeWhile(BigInt(n))
-  def iterativeWhile(n:Long):BigInt = iterativeWhile(BigInt(n))
+	def iterativeWhile(n:BigInt):BigInt = {
+		validate(n)
+		var i = n
+		var result = one
+		while (i > 0) {
+			result = result * i
+			i = i - one
+		}
+		result
+	}
+	def iterativeWhile(n:Int):BigInt = iterativeWhile(BigInt(n))
+	def iterativeWhile(n:Long):BigInt = iterativeWhile(BigInt(n))
 
-  def iterativeFor(n:BigInt):BigInt = {
-    validate(n)
-    var result = one
-    for (i <- one to n) { result *= i }
-    result
-  }
-  def iterativeFor(n:Int):BigInt = iterativeFor(BigInt(n))
-  def iterativeFor(n:Long):BigInt = iterativeFor(BigInt(n))
+	def iterativeFor(n:BigInt):BigInt = {
+		validate(n)
+		var result = one
+		for (i <- one to n) { result *= i }
+		result
+	}
+	def iterativeFor(n:Int):BigInt = iterativeFor(BigInt(n))
+	def iterativeFor(n:Long):BigInt = iterativeFor(BigInt(n))
 
-    def iterativeForeach(n:BigInt):BigInt = {
-    validate(n)
-    var result = one
-    (two to n) foreach ((i:BigInt) => result *= i)
-    result
-  }
-  def iterativeForeach(n:Int):BigInt = iterativeForeach(BigInt(n))
-  def iterativeForeach(n:Long):BigInt = iterativeForeach(BigInt(n))
+	def iterativeForeach(n:BigInt):BigInt = {
+		validate(n)
+		var result = one
+		(two to n) foreach ((i:BigInt) => result *= i)
+		result
+	}
+	def iterativeForeach(n:Int):BigInt = iterativeForeach(BigInt(n))
+	def iterativeForeach(n:Long):BigInt = iterativeForeach(BigInt(n))
 
-  def productive(n:BigInt):BigInt = {
-    validate(n)
-    (one to n).product
-  }
-  def productive(n:Int):BigInt = productive(BigInt(n))
-  def productive(n:Long):BigInt = productive(BigInt(n))
+	def productive(n:BigInt):BigInt = {
+		validate(n)
+			(one to n).product
+	}
+	def productive(n:Int):BigInt = productive(BigInt(n))
+	def productive(n:Long):BigInt = productive(BigInt(n))
 
-  def reductive(n:BigInt):BigInt = {
-    validate(n)
-    if (n < two) one
-    else two to n reduceLeft(_ * _)
-  }
-  def reductive(n:Int):BigInt = reductive(BigInt(n))
-  def reductive(n:Long):BigInt = reductive(BigInt(n))
+	def reductive(n:BigInt):BigInt = {
+		validate(n)
+		if (n < two) one
+		else two to n reduceLeft(_ * _)
+	}
+	def reductive(n:Int):BigInt = reductive(BigInt(n))
+	def reductive(n:Long):BigInt = reductive(BigInt(n))
 
-  def foldLeftive(n:BigInt):BigInt = {
-    validate(n)
-    (one /: (two to n))(_ * _)
-  }
-  def foldLeftive(n:Int):BigInt = foldLeftive(BigInt(n))
-  def foldLeftive(n:Long):BigInt = foldLeftive(BigInt(n))
+	def foldLeftive(n:BigInt):BigInt = {
+		validate(n)
+		(one /: (two to n))(_ * _)
+	}
+	def foldLeftive(n:Int):BigInt = foldLeftive(BigInt(n))
+	def foldLeftive(n:Long):BigInt = foldLeftive(BigInt(n))
 
-  def naïveRecursive(n:BigInt):BigInt = {
-    validate(n)
-    if (n < two) one
-    else n * naïveRecursive(n - 1)
-  }
-  def naïveRecursive(n:Int):BigInt = naïveRecursive(BigInt(n))
-  def naïveRecursive(n:Long):BigInt = naïveRecursive(BigInt(n))
+	def naïveRecursive(n:BigInt):BigInt = {
+		validate(n)
+		if (n < two) one
+		else n * naïveRecursive(n - 1)
+	}
+	def naïveRecursive(n:Int):BigInt = naïveRecursive(BigInt(n))
+	def naïveRecursive(n:Long):BigInt = naïveRecursive(BigInt(n))
 
-  def tailRecursive(n:BigInt):BigInt = {
-    validate(n)
-    if (n < two) one
-    else {
-      @tailrec
-      def iterate(x:BigInt = one, result:BigInt = one):BigInt =
-        if (x > n) result
-        else iterate(x + one, result * x)
-      iterate()
-    }
-  }
-  def tailRecursive(n:Int):BigInt = tailRecursive(BigInt(n))
-  def tailRecursive(n:Long):BigInt = tailRecursive(BigInt(n))
+	def tailRecursive(n:BigInt):BigInt = {
+		validate(n)
+		if (n < two) one
+		else {
+			@tailrec
+			def iterate(x:BigInt = one, result:BigInt = one):BigInt =
+				if (x > n) result
+				else iterate(x + one, result * x)
+			iterate()
+		}
+	}
+	def tailRecursive(n:Int):BigInt = tailRecursive(BigInt(n))
+	def tailRecursive(n:Long):BigInt = tailRecursive(BigInt(n))
 
 }
--- Scala/src/test/scala/uk/org/winder/maths/Factorial_ScalaTest_PropertyBased.scala
+++ Scala/src/test/scala/uk/org/winder/maths/Factorial_ScalaTest_PropertyBased.scala
@@ -7,26 +7,26 @@ import org.scalacheck.Gen
 
 class Factorial_ScalaTest_PropertyBased extends PropSpec with PropertyChecks {
 
-  val algorithms = Table(
-    ("algorithm", "name"),
-    (Factorial.iterativeWhile _, "iterativeWhile"),
-    (Factorial.iterativeFor _, "iterativeFor"),
-    (Factorial.iterativeForeach _, "iterativeForeach"),
-    (Factorial.productive _, "productive"),
-    (Factorial.reductive _, "reductive"),
-    (Factorial.foldLeftive _, "foldLeftive"),
-    (Factorial.naïveRecursive _, "naïveRecursive"),
-    (Factorial.tailRecursive _, "tailRecursive")
-  )
+	val algorithms = Table(
+		("algorithm", "name"),
+		(Factorial.iterativeWhile _, "iterativeWhile"),
+		(Factorial.iterativeFor _, "iterativeFor"),
+		(Factorial.iterativeForeach _, "iterativeForeach"),
+		(Factorial.productive _, "productive"),
+		(Factorial.reductive _, "reductive"),
+		(Factorial.foldLeftive _, "foldLeftive"),
+		(Factorial.naïveRecursive _, "naïveRecursive"),
+		(Factorial.tailRecursive _, "tailRecursive")
+	)
 
-  forAll(algorithms) { (f: Function1[Int, BigInt], name: String) =>
-    property(name + " obeys the recurrence relation for non-negative integers") {
-      val positiveIntSample = for (n <- Gen.choose(0, 1000)) yield n
-      forAll (positiveIntSample) { (n: Int) => assert(f(n + 1) == (n + 1) * f(n) ) }
-    }
-    property(name + " throws IllegalArgumentException for negative argument") {
-      forAll { (n:Int) => whenever (n < 0) { intercept [IllegalArgumentException] { f(n) } } }
-    }
-  }
+	forAll(algorithms) { (f: Function1[Int, BigInt], name: String) =>
+		property(name + " obeys the recurrence relation for non-negative integers") {
+			val positiveIntSample = for (n <- Gen.choose(0, 1000)) yield n
+			forAll (positiveIntSample) { (n: Int) => assert(f(n + 1) == (n + 1) * f(n) ) }
+		}
+		property(name + " throws IllegalArgumentException for negative argument") {
+			forAll { (n:Int) => whenever (n < 0) { intercept [IllegalArgumentException] { f(n) } } }
+		}
+	}
 
 }

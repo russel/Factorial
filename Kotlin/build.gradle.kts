8f727b1f77a207fe48c051b1c6e46f314db7e44b
Russel Winder <russel@winder.org.uk>
2019-01-04 17:54:28 +0000

    Kotlin internal dependencies are still broken.

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,3 +1,5 @@
+val kotlinVersion = "1.3.11"
+
 plugins {
 	kotlin("jvm") version("1.3.11")
 }
@@ -10,7 +12,9 @@ repositories {
 dependencies {
 	compile(kotlin("stdlib"))
 	// Need this for the KotlinTest to do the right thing.
-	testCompile("org.jetbrains.kotlin:kotlin-reflect:1.3.11")
+	testCompile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
+	// There seems to be a cock up in the Kotlin internal dependencies so we have to do this. :-(
+	testCompile("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
 	testCompile("io.kotlintest:kotlintest-runner-junit5:3.+")
 }
 
